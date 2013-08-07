48b53a62e8a161cbd5a5bd475590f316473009e1
SchizoDuckie <schizoduckie@gmail.com>
2013-08-07 23:37:55 +0200

    quick refactoring

--- blink.js
+++ blink.js
@@ -9,28 +9,14 @@ if (userAgentMatch && userAgentMatch.length > 1) {
 }
 
 if (userAgentMatch && userAgentMatch.length > 1 && firefoxVersion >= 23) {
-    var blinkTags = function () {
-        if(!blinkStart) {
-            blinkStart = new Date().getTime();
-            blinkNext = blinkStart;
-        }
-
-        blinkNext += 500;        
+    blinkTags = function () {
 
         var blinkElements = document.getElementsByTagName('blink');
 
-        if (blinkVisible === false) {
-            for (var i = 0; i < blinkElements.length; i++) {
-                blinkElements[i].style.visibility = 'visible';
-            }
-            blinkVisible = true;
-        } else {
-            for (var i = 0; i < blinkElements.length; i++) {
-                blinkElements[i].style.visibility = 'hidden';
-            }
-            blinkVisible = false;
+        for (var i = 0; i < blinkElements.length; i++) {
+            blinkElements[i].style.visibility = blinkElements[i].style.visibility == 'visible' ? 'hidden' : 'visible';
         }
-        setTimeout(blinkTags, blinkNext - new Date().getTime());
+        setTimeout(blinkTags, 500);
     };
 
     window.addEventListener('load',blinkTags,false);
